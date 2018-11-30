9b806229e5a8936e7875a3bcfad5640dec94eb54
ubogov <s.ubogov@skbkontur.ru>
2018-11-30 14:30:29 +0500

    done

---
+++ index.js
@@ -0,0 +1,42 @@
+const {sendFiles, getAllFilesWithExtInFolder} = require('ftp-client-wrapper');
+const args = require("args-parser")(process.argv);
+
+export default class SourcemapSender {
+	sourcemapsFolder;
+	assetsmapFolder;
+	destinationHost;
+	projectName;
+	projectVersion;
+
+	constructor(configPath) {
+		let {destinationHost, sourcemapsFolder, assetsmapFolder, projectName} = require(configPath);
+		this.sourcemapsFolder = args.sourcemapsFolder || sourcemapsFolder;
+		this.assetsmapFolder = args.assetsmapFolder || assetsmapFolder;
+		this.destinationHost = args.destinationHost || destinationHost;
+		this.projectName = args.projectName || projectName || 'default';
+		this.projectVersion = args.version || new Date().toLocaleString().replace(/:/g, '-').replace(/ /g, '_');
+
+
+	}
+
+	getAllFiles() {
+
+		const allSourcemaps = getAllFilesWithExtInFolder(this.sourcemapsFolder, 'map');
+		const allAssetsmaps = getAllFilesWithExtInFolder(this.assetsmapFolder, 'json');
+
+		return allSourcemaps.concat(allAssetsmaps);
+	}
+
+	send() {
+		const allFiles = this.getAllFiles();
+		sendFiles(allFiles, this.destinationHost, this.projectName, this.projectVersion);
+	}
+}
+
+
+
+
+
+
+
+
